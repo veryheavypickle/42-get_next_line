7414d905791dcf7c3f46a7d13c26af86aa5e78e5
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-08 00:23:44 +0200

    Set up test in main

--- main.c
+++ main.c
@@ -1,19 +1,13 @@
 #include "get_next_line.h"
 
-void	print(char *str)
-{
-	if (!s)
-		return ;
-	while (*str)
-		write(fd, s++, 1);
-}
-
 int main(void)
 {
-	char	*fileName;
+	char	*file;
 	int	fd;
 
-	fileName = "bee_movie.txt";
-	fd = open(file, O_RDONLY)
-	printf(get_next_line(fd));
+	file = "bee_movie.txt";
+	fd = open(file, O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	return (0);
 }
