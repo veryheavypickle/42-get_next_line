d1fa2105ae05207b79ace23631a6ebed9896080c
veryheavypickle <xavier.carroll01@gmail.com>
2022-07-08 00:23:02 +0200

    Made static variable to stay in memory, started read_line

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,21 @@
 #include "get_next_line.h"
 
+static char	*read_line(int line)
+{
+	char *str;
+	char *buffer;
+	int 	nl_count;
+
+	nl_count = 0;
+	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	read(fd, buffer, BUFFER_SIZE);
+	return (str);
+}
 char	*get_next_line(int fd)
 {
-	if (fd == -1)
+	static	int line = 0;
+	if (fd == -1 || BUFFER_SIZE < 1)
 		return (NULL);
+	line++;
 }
