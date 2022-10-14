e1a2fc51b7b4e58ed34b9d14c9ea8ae157d326e5
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-14 19:12:55 +0200

    removed everything

--- get_next_line.c
+++ get_next_line.c
@@ -4,31 +4,10 @@
  * 0 = False
  * 1 = True
  */
-static char	*read_line(int fd)
-{
-	static int	i;
-	static char *buf;
-	char		*str;
 
-	str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
 
-	//if (!buf)
-	//buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	//*/
-	if (!i)
-		i = 0;
-	if (!buf || !str)
-		return (NULL);
-	read(fd, buf, BUFFER_SIZE);
-	while (buf[i] != '\n' || buf[i])
-		i++;
-	return (str);
-}
 
 char	*get_next_line(int fd)
 {
-	//static	int line = 0;
-	if (fd == -1 || BUFFER_SIZE < 1)
-		return (NULL);
-	return (read_line(fd));
+
 }
