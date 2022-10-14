10d138c6d55004fa131ebd84d8f5b582aed1cda3
veryheavypickle <xavier.carroll01@gmail.com>
2022-10-14 20:03:11 +0200

    added error catching for read and entered while loop

--- get_next_line.c
+++ get_next_line.c
@@ -14,6 +14,7 @@
  * if read returns 0, then it is end of file
  * if read returns -1, then there has been an error
  * I may not even use num_bytes but this depends on testers
+ * num_bytes is set to 1 so it can enter the while loop
  *
  * while there isn't a new line character in the line, keep reading
  * always add null to end of string
@@ -24,12 +25,15 @@ char	*read_line(int fd, char* line)
 	char	*buf;
 	int 	num_bytes;
 
+	num_bytes = 1;
 	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buf)
 		return (NULL);
 	while(!ft_strchr(line, '\n') && num_bytes > 0)
 	{
 		num_bytes = read(fd, buf, BUFFER_SIZE);
+		if (num_bytes < 0)
+			return (NULL);
 		buf[num_bytes] = '\0';
 		line = ft_strjoin(line, buf);
 	}
