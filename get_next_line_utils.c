57ae535040614c6106e3450843c434a5a7906fe4
veryheavypickle <xavier.carroll01@gmail.com>
2022-11-20 16:40:37 +0100

    merged strjoin and memcpy, added line cleaners

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,6 +1,5 @@
 #include "get_next_line.h"
 
-/* copied from the libft - modified to initialise string if needed */
 char	*ft_strjoin(char *s1, char const *s2)
 {
 	size_t	len_s1;
@@ -14,14 +13,17 @@ char	*ft_strjoin(char *s1, char const *s2)
 	}
 	if (!s2)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	str = (char *)malloc(sizeof(char) * (len_s1 + len_s2 + 1));
+	str = malloc(sizeof(char) * ((ft_strlen(s1) + ft_strlen(s2)) + 1));
 	if (!str)
 		return (NULL);
-	ft_memcpy(str, s1, len_s1);
-	ft_memcpy(str + len_s1, s2, len_s2);
-	str[len_s1 + len_s2] = '\0';
+	len_s1 = -1;
+	len_s2 = 0;
+	while (s1[++len_s1] != '\0')
+		str[len_s1] = s1[len_s1];
+	while (s2[len_s2] != '\0')
+		str[len_s1++] = s2[len_s2++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
 	return (str);
 }
 
@@ -33,37 +35,6 @@ size_t	ft_strlen(const char *s)
 	while (s[i])
 		i++;
 	return (i);
-}
-
-void	*ft_memcpy(void *dest_any, const void *src_any, size_t size)
-{
-	int	i;
-
-	i = 0;
-	if ((!dest_any && !src_any) || !size)
-		return (dest_any);
-	while (size--)
-	{
-		((char *)dest_any)[i] = ((char *)src_any)[i];
-		i++;
-	}
-	return (dest_any);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	len;
-
-	len = ft_strlen(src);
-	if (!size)
-		return (len);
-	while (*src && size - 1)
-	{
-		*dst++ = *src++;
-		size--;
-	}
-	*dst = '\0';
-	return (len);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -84,4 +55,61 @@ char	*ft_strchr(const char *s, int c)
 	if (c == 0)
 		return ((char *)(s + i));
 	return (NULL);
+}
+
+/*
+ * Cleans line
+ */
+char	*ft_clean_line(char *line)
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	if (!line[i])
+		return (NULL);
+	while (line[i] && line[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (line[i] && line[i] != '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	if (line[i] == '\n')
+	{
+		str[i] = line[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_clean_static_line(char *line)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (!line[i])
+	{
+		free(line);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(line) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (line[i])
+		str[j++] = line[i++];
+	str[j] = '\0';
+	free(line);
+	return (str);
 }
